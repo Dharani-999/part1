abb35e3bb5d0dc3c8dc77ee4e89b3be510964232
Dharani-999 <74001250+Dharani-999@users.noreply.github.com>
2021-06-01 21:55:06 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,62 +1,75 @@
-import React from 'react'
 
-const Header = ({course}) => {
+import React, {useState} from 'react';
+
+
+const Statistic = (props) => {
     return (
-        <h1>{course}</h1>
-    );
-};
+        <p>{props.text} {props.value}</p>
+    )
+}
 
-const Part = ({part}) => {
-    return (
-        <p>
-            {part.name} {part.exercises}
-        </p>
-    );
-};
-
-const Content = ({parts}) => {
+const Statistics = (props) => {
+    const all = props.good + props.bad + props.neutral
+    const average = (props.good + (props.bad*-1))/all
+    const positive = (props.good)/all *100
+    if(all===0){
+        return (
+            <p>No feedbacks given</p>
+        )
+    }
     return (
         <div>
-            <Part part={parts[0]} />
-            <Part part={parts[1]} />
-            <Part part={parts[2]} />
+            <table>
+                <tbody>
+                    <tr>
+                        <td><Statistic text="good" value={props.good} /></td>
+                    </tr>
+                    <tr>
+                        <td><Statistic text="neutral" value={props.neutral} /></td>
+                    </tr>
+                    <tr>
+                        <td><Statistic text="bad" value={props.bad} /></td>
+                    </tr>
+                    <tr>
+                        <td><Statistic text="all" value={all} /></td>
+                    </tr>
+                    <tr>
+                        <td><Statistic text="average" value={average} /></td>
+                    </tr>
+                    <tr>
+                        <td><Statistic text="positive" value={positive} /></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-    );
-};
+    )
+}
 
-const Total = ({parts}) => {
+const Button = (props) => {
     return (
-        <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-    );
-};
-
+        <button onClick={props.event}>{props.text}</button>
+    )
+}
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-            name: 'Fundamentals of React',
-            exercises: 10
-            },
-            {
-            name: 'Using props to pass data',
-            exercises: 7
-            },
-            {
-            name: 'State of a component',
-            exercises: 14
-            }
-        ]
-    };
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
+    
     return (
         <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            <h1> Dear customer, Please give your valuable feedback </h1>
+            <div>
+                <Button event={()=> setGood(good+1)} text="Good" />
+                <Button event={()=> setNeutral(neutral+1)} text="Neutral" />
+                <Button event={()=> setBad(bad+1)} text="Bad" />
+                                
+            </div>
+            <h2>statistics</h2>
+            <Statistics good={good} bad ={bad} neutral={neutral} />
         </div>
-    );
-};
+    )
+}
 
 
 
